f175c4bafc840a3589a0483f8cbeec24e1b8cdfb
lhs <fantasytotal@naver.com>
2025-01-03 11:59:50 +0900

    [feat] 로그인/자동 로그인 구현

--- lib/data/gvm/session_gvm.dart
+++ lib/data/gvm/session_gvm.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_blog/_core/utils/my_http.dart';
 import 'package:flutter_blog/data/repository/user_repository.dart';
 import 'package:flutter_blog/main.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
@@ -9,7 +10,7 @@ class SessionUser {
   String? accessToken;
   bool? isLogin;
 
-  SessionUser({this.id, this.username, this.accessToken, this.isLogin});
+  SessionUser({this.id, this.username, this.accessToken, this.isLogin = false});
 }
 
 class SessionGVM extends Notifier<SessionUser> {
@@ -23,7 +24,36 @@ class SessionGVM extends Notifier<SessionUser> {
         id: null, username: null, accessToken: null, isLogin: false);
   }
 
-  Future<void> login() async {}
+  Future<void> login(String username, String password) async {
+    final body = {
+      "username": username,
+      "password": password,
+    };
+
+    final (Map<String, dynamic> responseBody, String accessToken) =
+        await userRepository.findByUsernameAndPassword(body);
+    if (!responseBody["success"]) {
+      ScaffoldMessenger.of(mContext!).showSnackBar(
+        SnackBar(content: Text("회원가입 실패 : ${responseBody["errorMessage"]}")),
+      );
+      return;
+    }
+    // 1. 토큰을 Storage 저장
+    await secureStorage.write(key: "accessToken", value: accessToken);
+
+    // 2. SessionUser 갱신
+    Map<String, dynamic> data = responseBody["response"];
+    state = SessionUser(
+        id: data["id"],
+        username: data["username"],
+        accessToken: accessToken,
+        isLogin: true);
+    // 3. Dio 토큰 세팅
+    dio.options.headers = {"Authorization": accessToken};
+    // Logger().d(dio.options.headers);
+
+    Navigator.popAndPushNamed(mContext, "/post/list");
+  }
 
   Future<void> join(String username, String email, String password) async {
     final body = {
@@ -39,19 +69,50 @@ class SessionGVM extends Notifier<SessionUser> {
       );
       return;
     }
-
-    Navigator.pushNamed(mContext, "/login");
+    Navigator.pop(mContext);
+    // Navigator.popAndPushNamed(mContext, "/login");
   }
 
-  Future<void> logout() async {}
+  Future<void> logout() async {
+    // 1. 디바이스 토큰 삭제
+    await secureStorage.delete(key: "accessToken");
 
+    // 2. 상태 갱신
+    state = SessionUser();
+
+    // 3. 화면 이동
+    Navigator.popAndPushNamed(mContext, "/login");
+  }
+
+  // 1. 절대 SessionUser가 있을 수 없다.
   Future<void> autoLogin() async {
-    Future.delayed(
-      Duration(seconds: 3),
-      () {
-        Navigator.popAndPushNamed(mContext, "/login");
-      },
-    );
+    // 1. 토큰 디바이스에서 가져오기
+    String? accessToken = await secureStorage.read(key: "accessToken");
+
+    if (accessToken == null) {
+      Navigator.popAndPushNamed(mContext, "/login");
+    }
+
+    Map<String, dynamic> responseBody =
+        await userRepository.autoLogin(accessToken);
+
+    if (!responseBody["success"]) {
+      Navigator.popAndPushNamed(mContext, "/login");
+      return;
+    }
+
+    // 2. SessionUser 갱신
+    Map<String, dynamic> data = responseBody["response"];
+    state = SessionUser(
+        id: data["id"],
+        username: data["username"],
+        accessToken: accessToken,
+        isLogin: true);
+
+    // 3. Dio 토큰 세팅
+    dio.options.headers = {"Authorization": accessToken};
+
+    Navigator.popAndPushNamed(mContext, "/post/list");
   }
 }
 
--- lib/data/repository/user_repository.dart
+++ lib/data/repository/user_repository.dart
@@ -12,4 +12,31 @@ class UserRepository {
     // Logger().d(body);
     return body;
   }
+
+  Future<(Map<String, dynamic>, String)> findByUsernameAndPassword(
+      Map<String, String> data) async {
+    Response response = await dio.post("/login", data: data);
+
+    Map<String, dynamic> body = response.data;
+    // Logger().d(body);
+
+    String accessToken = "";
+    try {
+      accessToken = response.headers["Authorization"]![0];
+      // Logger().d(accessToken);
+    } catch (e) {}
+
+    return (body, accessToken);
+  }
+
+  Future<Map<String, dynamic>> autoLogin(String? accessToken) async {
+    Response response = await dio.post(
+      "/auto/login",
+      options: Options(headers: {"Authorization": accessToken}),
+    );
+
+    Map<String, dynamic> body = response.data;
+
+    return body;
+  }
 }
